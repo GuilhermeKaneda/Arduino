de1049921ccf3ab07688b904b26b7aa230daa73a
Guilherme <l0rd.v4dher@gmail.com>
2024-07-29 19:29:27 -0300

    arduino exercises

--- Analogico/Ex1.ino
+++ Analogico/Ex1.ino
@@ -1,7 +1,6 @@
 int led = 3;
 int pont = A0;
 int val;
-int time;
 
 void setup(){
 	pinMode(led, OUTPUT);
@@ -9,12 +8,17 @@ void setup(){
 }
 
 void loop(){
-    val = analogRead(pont);
-    val = map(val, 0, 1023, 0, 4);
 	for(int i = 0; i <= 255; i++) {
         analogWrite(led, i);
+        Serial.println(i);
     }
+    delay(500);
+
     for(int i = 255; i >= 0; i--) {
         analogWrite(led, i);
+        Serial.println(i);
+
     }
+
+    delay(500);
 }
